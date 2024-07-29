60e7ba120dbb614678ae2f94eedfee44cd46c16e
Dida P <didadida135@gmail.com>
2024-07-29 00:26:42 -0400

    Fixed gains (possibly wrong?

--- Parameters.m
+++ Parameters.m
@@ -6,10 +6,19 @@ L_d = L_q; %H
 R_s= 7.06E-3; % Ohm
 Flux = 0.03737;  %Wb
 p = 4;
-Kp = 143;
-Ki = 1300;
-Ki_s = 1;
-Kp_s = 1;
+
+%Calculating Gains
+fn=R_s/(2*pi*L_q);
+Kpq=(L_q*2*pi*fn)/10;
+Kpd=(L_d*2*pi*fn)/10;
+Kiq=Kpd*(R_s/L_q);
+Kid=Kpd*(R_s/L_d);
+
+
+%Kp = 143;
+%Ki = 1300;
+%Ki_s = 1;
+%Kp_s = 1;
 
 
 
