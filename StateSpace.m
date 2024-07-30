ebaa5783110eee2b5edb640afa77a86d61baa8ce
didadida135 <didadida135@gmail.com>
2024-07-30 12:02:08 -0400

    Working Torque Tracking, 2 versions. Plus a working step response function for statespace

--- StateSpace.m
+++ StateSpace.m
@@ -1,23 +1,47 @@
-% Define symbolic variables and parameters
-syms id iq vd vq real
-syms R L Psi_f p real
+clear
 
-% Operating point
-I_q = 2.0; % Example operating current in q-axis
-omega_m0 = 100; % Example operating mechanical speed
+% Define symbolic variables and parameters
+Lq = 0.000795; %H
+Ld = Lq; %H
+Rs= 0.05; % Ohm
+Flux = 0.192;  %Wb
+p = 4;
+vdc=560;
+
+%Calculating Gains
+fsw=5000;
+Kpq=(Lq*2*pi*fsw)/10;
+Kpd=(Ld*2*pi*fsw)/10;
+Kiq=Kpd*(Rs/Lq);
+Kid=Kpd*(Rs/Ld);
+Ksq=Kiq;
+Ksd=Kid;
+
 
 % Define linearized state-space matrices
-A = [-R/L, -omega_m0;omega_m0, -R/L];
+A = [-Rs/Ld, Lq/Ld;
+    -Ld/Lq, -Rs/Lq]; % *K matrix
+    
+B = [1/Ld, 0;
+     0, 1/Lq];
 
-B = [1/L, 0;
-     0, 1/L];
+% E how do we add this one?
+E = [0;      % *wm
+    Flux/Lq]; 
 
 % Assuming Psi_f is linearly approximated or given as a constant
 C = eye(2); % Output all states
 D = zeros(2,2); % No direct feedthrough from inputs to outputs
 
+%Controller Matrix
+K= [0,1;
+    1,0]; % *w_m
+
+%Obs = ------
+
 % Create state-space model
 sys = ss(A, B, C, D);
+step(sys);
 
 % Display state-space matrices
 disp('State-space matrices:');
