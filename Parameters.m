f3f4f0320a0bd5736159445981a9650f218ebb87
didadida135 <didadida135@gmail.com>
2024-07-29 20:31:00 -0400

    Working Torque/Speed Control When selected individually

--- Parameters.m
+++ Parameters.m
@@ -1,24 +1,27 @@
 clear
 
 % Define motor parameters
-L_q = 96.5E-6; %H
+L_q = 0.000795; %H
 L_d = L_q; %H
-R_s= 7.06E-3; % Ohm
-Flux = 0.03737;  %Wb
+R_s= 0.05; % Ohm
+Flux = 0.192;  %Wb
 p = 4;
+vdc=560;
+Vdc=100;
 
 %Calculating Gains
-fsw=2000;
+fsw=5000;
 Kpq=(L_q*2*pi*fsw)/10;
 Kpd=(L_d*2*pi*fsw)/10;
 Kiq=Kpd*(R_s/L_q);
 Kid=Kpd*(R_s/L_d);
+Ksq=Kiq;
+Ksd=Kid;
 
-
-%Kp = 143;
-%Ki = 1300;
-%Ki_s = 1;
-%Kp_s = 1;
+Kp = (L_q*2*pi*fsw)/10;
+Ki = (L_d*2*pi*fsw)/10;
+Ki_s = 1;
+Kp_s = 1;
 
 
 
